cd2b3713f4fe9e304d3df54b1e4e80faec66e4c6
sabertazimi <sabertazimi@gmail.com>
2021-08-08 19:35:00 +0800

    test(CLI-create): add create command basic testing

--- lib/index.test.ts
+++ lib/index.test.ts
@@ -1,4 +1,23 @@
+import { CreateCommand } from './index';
+import inquirer from 'inquirer';
+import PromptUI from 'inquirer/lib/ui/prompt';
+
+const appName = '.temp';
+
 describe('Create', () => {
-  test.todo('should fetch boilerplate from GitHub');
+  test('should fetch boilerplate from GitHub when [simple] command)', () => {
+    const mockPrompt = jest.spyOn(inquirer, 'prompt').mockImplementation(() => {
+      const promise = new Promise((resolve) => {
+        resolve({ templateAction: 'simple' });
+      });
+      return promise as Promise<unknown> & { ui: PromptUI };
+    });
+
+    const createCommand = new CreateCommand();
+    createCommand.run(appName);
+
+    mockPrompt.mockRestore();
+  });
+
   test.todo('should generate boilerplate via create-react-app');
 });
