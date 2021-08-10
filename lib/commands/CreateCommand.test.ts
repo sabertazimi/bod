137bda47da211d9ec587eabb9c31acde699983d5
sabertazimi <sabertazimi@gmail.com>
2021-08-11 00:43:29 +0800

    test(CLI-CreateCommand): add error handle testing

--- lib/commands/CreateCommand.test.ts
+++ lib/commands/CreateCommand.test.ts
@@ -1,3 +1,6 @@
+import { SpawnSyncReturns } from 'child_process';
+import { isCI } from 'ci-info';
+import spawn from 'cross-spawn';
 import inquirer from 'inquirer';
 import PromptUI from 'inquirer/lib/ui/prompt';
 import rimraf from 'rimraf';
@@ -7,8 +10,8 @@ const appPath = 'temp';
 
 describe('Create', () => {
   test.each(CreateCommand.TemplateActions)(
-    'should initialize app directory according to template choice [$name]',
-    ({ value }) => {
+    'should initialize app directory via template choice [$name]',
+    async ({ value }) => {
       const mockPrompt = jest
         .spyOn(inquirer, 'prompt')
         .mockImplementation(() => {
@@ -18,12 +21,39 @@ describe('Create', () => {
           return promise as Promise<unknown> & { ui: PromptUI };
         });
 
-      rimraf.sync(appPath);
       const createCommand = new CreateCommand();
-      createCommand.run(appPath);
+      if (isCI) {
+        await createCommand.run(appPath);
+      }
 
       rimraf.sync(appPath);
       mockPrompt.mockRestore();
+    }
+  );
+
+  test.each(CreateCommand.TemplateActions)(
+    'should throw error when exited with non zero via template choice [$name]',
+    async ({ value }) => {
+      const mockPrompt = jest
+        .spyOn(inquirer, 'prompt')
+        .mockImplementation(() => {
+          const promise = new Promise((resolve) => {
+            resolve({ templateName: value });
+          });
+          return promise as Promise<unknown> & { ui: PromptUI };
+        });
+      const mockSpawn = jest.spyOn(spawn, 'sync').mockImplementation(() => {
+        return {
+          status: 1,
+        } as SpawnSyncReturns<Buffer>;
+      });
+
+      const createCommand = new CreateCommand();
+      await expect(createCommand.run(appPath)).rejects.toThrowError();
+
+      rimraf.sync(appPath);
+      mockPrompt.mockRestore();
+      mockSpawn.mockRestore();
     }
   );
 });
