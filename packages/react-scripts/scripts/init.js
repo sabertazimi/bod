20f788913eb47b7da5322d52d5f6064eb3cb83fa
sabertazimi <sabertazimi@gmail.com>
2021-08-12 04:14:23 +0800

    fix(CRA-scripts): hidden eject scripts by default

--- packages/react-scripts/scripts/init.js
+++ packages/react-scripts/scripts/init.js
@@ -187,7 +187,6 @@ module.exports = function (
       start: 'react-scripts start',
       build: 'react-scripts build',
       test: 'react-scripts test',
-      eject: 'react-scripts eject',
     },
     templateScripts
   );
