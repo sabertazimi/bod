79f65f82978e7205d473f241c9385e86a4c2ab13
sabertazimi <sabertazimi@gmail.com>
2021-08-12 04:09:49 +0800

    fix(CRA-template): rectify reference types path for template

--- packages/react-scripts/scripts/utils/verifyTypeScriptSetup.js
+++ packages/react-scripts/scripts/utils/verifyTypeScriptSetup.js
@@ -290,7 +290,7 @@ function verifyTypeScriptSetup() {
   if (!fs.existsSync(paths.appTypeDeclarations)) {
     fs.writeFileSync(
       paths.appTypeDeclarations,
-      `/// <reference types="react-scripts" />${os.EOL}`
+      `/// <reference types="@sabertazimi/react-scripts" />${os.EOL}`
     );
   }
 }
