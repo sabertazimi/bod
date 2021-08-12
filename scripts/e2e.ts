357f677e4ba8a2cf25c16e8460c758aa08fbd68b
sabertazimi <sabertazimi@gmail.com>
2021-08-13 06:59:14 +0800

    test(CRA-e2e): publish to local registry
    issue #14

--- scripts/e2e.ts
+++ scripts/e2e.ts
@@ -7,30 +7,34 @@ import path from 'path';
 
 class Test {
   appName: string;
+  cwd: string;
   rootPath: string;
   packagesPath: string;
-  cwd: string;
+  localPort: number;
   localRegistry: string;
-  originalRegistry: string;
   localRegistryConfigPath: string;
   localRegistryLogPath: string;
 
   constructor(appName: string) {
     this.appName = appName;
-    this.rootPath = '';
-    this.packagesPath = '';
-    this.cwd = '';
-    this.localRegistry = '';
-    this.originalRegistry = '';
-    this.localRegistryConfigPath = '';
-    this.localRegistryLogPath = '';
+    this.cwd = process.cwd();
+    consola.info(`Working in directory ${this.cwd}.`);
+    this.rootPath = path.join(__dirname, '..');
+    this.packagesPath = path.join(this.rootPath, 'packages');
+    this.localPort = 4873;
+    this.localRegistry = `http://localhost:${this.localPort}`;
+    this.localRegistryConfigPath = path.join(
+      this.rootPath,
+      'scripts/verdaccio.yaml'
+    );
+    this.localRegistryLogPath = path.join(this.rootPath, 'registry.log');
   }
 
-  static exec(command: string, output?: boolean, cwd?: string) {
+  static exec(command: string, cwd?: string) {
     return cp.execSync(command, {
       shell: '/usr/bin/bash',
-      stdio: output ? 'inherit' : 'pipe',
       cwd: cwd ?? process.cwd(),
+      stdio: 'inherit',
     });
   }
 
@@ -39,21 +43,19 @@ class Test {
     Test.exec(
       `nohup npx verdaccio -c ${this.localRegistryConfigPath} &>${this.localRegistryLogPath} &`
     );
-    Test.exec(
-      `grep -q 'http address' <(tail -f ${this.localRegistryLogPath})`,
-      true
-    );
-    Test.exec(`npm set registry "${this.localRegistry}"`, true);
-    consola.info(`Setup local registry ${this.localRegistry}.`);
+    Test.exec(`grep -q 'http address' <(tail -f ${this.localRegistryLogPath})`);
   }
 
   stopLocalRegistry() {
-    Test.exec(`npm set registry ${this.originalRegistry}`, true);
+    Test.exec(`kill $(lsof -t -i:${this.localPort})`);
   }
 
   publishToLocalRegistry() {
     // Test.exec('git clean -df');
-    Test.exec('npm run publish', true);
+    Test.exec(
+      `npx npm-auth-to-token -u test -p test -e test@test.com -r ${this.localRegistry}`
+    );
+    Test.exec(`npm publish -ws --registry ${this.localRegistry}`);
   }
 
   cleanUp() {
@@ -128,32 +130,15 @@ class Test {
     // npx create-react-app appName --template @sabertazimi/typescript --scripts-version @sabertazimi/react-scripts
     Test.exec(
       `npx create-react-app ${this.appName} --scripts-version ${scriptsPath}`,
-      true,
       this.rootPath
     );
-  }
-
-  init() {
-    this.cwd = process.cwd();
-    consola.info(`Working in directory ${this.cwd}.`);
-
-    this.rootPath = path.join(__dirname, '..');
-    this.packagesPath = path.join(this.rootPath, 'packages');
-    this.originalRegistry = Test.exec('npm get registry').toString();
-    this.localRegistry = 'http://localhost:4873';
-    this.localRegistryConfigPath = path.join(
-      this.rootPath,
-      'scripts/verdaccio.yaml'
-    );
-    this.localRegistryLogPath = path.join(this.rootPath, 'registry.log');
   }
 
   run() {
     this.handleSetup();
     this.checkGitStatus();
-    this.init();
     this.startLocalRegistry();
-    // this.publishToLocalRegistry();
+    this.publishToLocalRegistry();
     this.handleExit();
   }
 }
