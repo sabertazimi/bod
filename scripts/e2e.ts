6a305db647d1089fc3a67de1603d2268a919ce01
sabertazimi <sabertazimi@gmail.com>
2021-08-14 00:38:59 +0800

    fix(CRA-e2e): pretty e2e testing output
    issue #14

--- scripts/e2e.ts
+++ scripts/e2e.ts
@@ -22,10 +22,7 @@ class Test {
     consola.info(`Working in directory ${this.cwd}.`);
     this.rootPath = path.join(__dirname, '..');
     this.packagesPath = path.join(this.rootPath, 'packages');
-    this.originalRegistry = Test.exec(
-      'npm config get registry',
-      true
-    ).toString();
+    this.originalRegistry = Test.execPipe('npm config get registry').toString();
     this.localPort = 4873;
     this.localRegistry = `http://localhost:${this.localPort}`;
     this.localRegistryConfigPath = path.join(
@@ -35,10 +32,19 @@ class Test {
     this.localRegistryLogPath = path.join(this.rootPath, 'registry.log');
   }
 
-  static exec(command: string, pipe?: boolean, cwd?: string) {
+  static exec(command: string, cwd?: string) {
+    consola.info(`  [exec]: ${command}.`);
     return cp.execSync(command, {
       shell: '/usr/bin/bash',
-      stdio: pipe ? 'pipe' : 'inherit',
+      stdio: 'inherit',
+      cwd: cwd ?? process.cwd(),
+    });
+  }
+
+  static execPipe(command: string, cwd?: string) {
+    return cp.execSync(command, {
+      shell: '/usr/bin/bash',
+      stdio: 'pipe',
       cwd: cwd ?? process.cwd(),
     });
   }
@@ -53,6 +59,7 @@ class Test {
   }
 
   stopLocalRegistry() {
+    consola.info('Clear local registry ...');
     const localRegistryAuthStorage = path.join(
       this.rootPath,
       'scripts/htpasswd'
@@ -63,28 +70,26 @@ class Test {
     );
     const localRegistryMetaStorage = path.join(this.rootPath, 'storage');
     Test.exec(`npm config set registry "${this.originalRegistry}"`);
-    consola.info('Reset npm registry.');
     Test.exec(`kill -9 $(lsof -t -i:${this.localPort})`);
     Test.exec(`rm -fr ${localRegistryAuthStorage}`);
     Test.exec(`rm -fr ${localRegistryBundleStorage}`);
     Test.exec(`rm -fr ${localRegistryMetaStorage}`);
-    consola.info('Clear all local registry storage files.');
   }
 
   publishToLocalRegistry() {
+    consola.info(`Publish packages to ${this.localRegistry} ...`);
     Test.exec('git clean -df');
     Test.exec(
       `npx npm-auth-to-token -u test -p test -e test@test.com -r ${this.localRegistry}`
     );
     Test.exec('npx standard-version --skip.changelog --skip.commit --skip.tag');
-    consola.info(`Publish packages to ${this.localRegistry} ...`);
-    Test.exec(`npm publish -ws`, true);
+    Test.exec(`npm publish -ws`);
   }
 
   cleanUp() {
     consola.info('Cleaning up ...');
     Test.exec(
-      'git checkout -- package.json package-lock.json packages/*/package.json'
+      'git checkout -- .npmrc package.json package-lock.json packages/*/package.json'
     );
     this.stopLocalRegistry();
   }
@@ -107,12 +112,12 @@ class Test {
     consola.error('An error was encountered while executing');
     consola.error(error);
     this.cleanUp();
-    consola.info('Exiting with error.');
+    consola.error('Exiting with error.');
     process.exit(1);
   }
 
   checkGitStatus() {
-    const gitStatus = Test.exec('git status --porcelain', true).toString();
+    const gitStatus = Test.execPipe('git status --porcelain').toString();
 
     if (gitStatus.trim() !== '') {
       consola.info('Please commit your changes before running this script!');
@@ -139,8 +144,10 @@ class Test {
   }
 
   packReactScripts(packagesPath: string) {
-    const scriptsFileName = cp
-      .execSync('npm pack', { cwd: path.join(packagesPath, 'react-scripts') })
+    const scriptsFileName = Test.execPipe(
+      'npm pack',
+      path.join(packagesPath, 'react-scripts')
+    )
       .toString()
       .trim();
     const scriptsPath = path.join(
@@ -155,7 +162,6 @@ class Test {
     // npx create-react-app appName --template @sabertazimi/typescript --scripts-version @sabertazimi/react-scripts
     Test.exec(
       `npx create-react-app ${this.appName} --scripts-version ${scriptsPath}`,
-      false,
       this.rootPath
     );
   }
