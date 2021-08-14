cf5ced85a272f4115023eec09e05f54446bc7c80
sabertazimi <sabertazimi@gmail.com>
2021-08-14 21:04:25 +0800

    feat(bod-CLI): register commands with `Iterator` pattern
    close #15

--- packages/bod/lib/bod.ts
+++ packages/bod/lib/bod.ts
@@ -5,11 +5,7 @@ import { Command, program } from 'commander';
 import consola from 'consola';
 import fs from 'fs';
 import path from 'path';
-import { BaseCommand } from './commands';
 import { CommandFactory } from './index';
-
-const createCommand = CommandFactory.get('create') as BaseCommand;
-const infoCommand = CommandFactory.get('info') as BaseCommand;
 
 const packageJsonPath = path.join(__dirname, '../package.json');
 const packageJson = JSON.parse(
@@ -19,30 +15,19 @@ const packageJson = JSON.parse(
 program.version(packageJson.version, '-v, --version');
 program.usage('<command> [options]');
 
-program
-  .command(createCommand.getUsage())
-  .description(createCommand.getDescription())
-  .action(async (appName: string) => {
-    try {
-      await createCommand.run(appName);
-    } catch (error) {
-      consola.error(error);
-      consola.error('Bod create failed.');
-      program.outputHelp();
-    }
-  });
-
-program
-  .command(infoCommand.getUsage())
-  .description(infoCommand.getDescription())
-  .action(async () => {
-    try {
-      await infoCommand.run();
-    } catch (error) {
-      consola.error(error);
-      program.outputHelp();
-    }
-  });
+for (const command of CommandFactory.values()) {
+  program
+    .command(command.getUsage())
+    .description(command.getDescription())
+    .action(async (appName: string) => {
+      try {
+        await command.run(appName);
+      } catch (error) {
+        consola.error(error);
+        program.outputHelp();
+      }
+    });
+}
 
 // output help information on unknown commands
 program.arguments('<command> [options]').action((command: string) => {
