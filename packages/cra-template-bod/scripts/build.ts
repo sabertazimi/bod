f990509184e5aee873027bf2e3c967583e7d2ca5
sabertazimi <sabertazimi@gmail.com>
2021-08-30 21:53:08 +0800

    build(template-bod): watch and rebuild `template` under local development

--- packages/cra-template-bod/scripts/build.ts
+++ packages/cra-template-bod/scripts/build.ts
@@ -1,85 +1,8 @@
-import chalk from 'chalk';
-import cp from 'child_process';
-import fs from 'fs';
-import path from 'path';
+import { buildJson, buildTemplate } from './utils';
 
-const rootPath = path.join(__dirname, '..');
-
-const exec = (cmd: string) => {
-  console.info(`    ${chalk.bgGreen.black('[exec]')}: ${cmd}`);
-  return cp.execSync(cmd, {
-    shell: '/usr/bin/bash',
-    stdio: 'inherit',
-    cwd: rootPath,
-  });
-};
-
-const buildTemplate = () => {
-  exec('rm -rf template');
-  exec('mkdir -p template');
-  exec('cp -fr public template/');
-  exec('cp -fr src template/');
-  exec('cp -fr tsconfig.json template/');
-  exec('cp -fr README.md template/');
-  exec('cp -fr .gitignore template/gitignore');
-};
-
-const buildJson = () => {
-  const packageJson = JSON.parse(
-    fs.readFileSync(path.join(rootPath, 'package.json'), 'utf8')
-  );
-
-  const ignoreDeps = [
-    '@sabertazimi/react-scripts',
-    'react-scripts',
-    'react',
-    'react-dom',
-  ];
-
-  // Remove ignored dependencies
-  const appDeps = Object.keys(packageJson.dependencies)
-    .filter((dep: string) => !ignoreDeps.includes(dep))
-    .reduce((deps: { [key: string]: string }, dep: string) => {
-      deps[dep] = packageJson.dependencies[dep];
-      return deps;
-    }, {});
-
-  // Remove ignored devDependencies
-  const appDevDeps = Object.keys(packageJson.devDependencies)
-    .filter((dep: string) => !ignoreDeps.includes(dep))
-    .reduce((deps: { [key: string]: string }, dep: string) => {
-      deps[dep] = packageJson.devDependencies[dep];
-      return deps;
-    }, {});
-
-  // Keep 'template:xxx' scripts
-  const appScripts = Object.keys(packageJson.scripts)
-    .filter((script: string) => script.startsWith('template:'))
-    .reduce((scripts: { [key: string]: string }, script: string) => {
-      const scriptName = script.slice(script.indexOf(':') + 1);
-      scripts[scriptName] = packageJson.scripts[script];
-      return scripts;
-    }, {});
-
-  const appPackageJson = {
-    package: {
-      ...packageJson,
-      dependencies: { ...appDeps },
-      devDependencies: { ...appDevDeps },
-      scripts: { ...appScripts },
-    },
-  };
-
-  // Write updated package.json
-  fs.writeFileSync(
-    path.join(rootPath, 'template.json'),
-    JSON.stringify(appPackageJson, null, 2)
-  );
-};
-
-const main = () => {
+const build = () => {
   buildTemplate();
   buildJson();
 };
 
-main();
+build();
---
+++ packages/cra-template-bod/scripts/watch.ts
@@ -0,0 +1,34 @@
+import chokidar from 'chokidar';
+import consola from 'consola';
+import path from 'path';
+import { buildJson, exec, rootPath } from './utils';
+
+chokidar
+  .watch([
+    path.join(rootPath, 'public'),
+    path.join(rootPath, 'src'),
+    path.join(rootPath, 'tsconfig.json'),
+    path.join(rootPath, 'README.md'),
+    path.join(rootPath, '.gitignore'),
+    path.join(rootPath, 'package.json'),
+  ])
+  .on('change', file => {
+    try {
+      if (path.resolve(file) === path.join(path.join(rootPath, '.gitignore'))) {
+        exec('cp -fr .gitignore template/gitignore');
+      } else if (
+        path.resolve(file) === path.join(path.join(rootPath, 'package.json'))
+      ) {
+        buildJson();
+      } else {
+        exec(
+          `cp -fr ${path.resolve(file)} ${path.join(
+            'template',
+            path.relative(rootPath, path.resolve(file))
+          )}`
+        );
+      }
+    } catch (err) {
+      if (err instanceof Error) consola.error(err.message);
+    }
+  });
