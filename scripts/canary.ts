d6ada54af3a0c47fe01a7d6fb0c977a19a9a84c6
sabertazimi <sabertazimi@gmail.com>
2021-09-02 23:56:55 +0800

    build(CI-canary): remove commit hash from canary release version

--- scripts/canary.ts
+++ scripts/canary.ts
@@ -5,11 +5,7 @@ const main = () => {
     .execPipe('git rev-list --count HEAD')
     .toString()
     .replace('\n', '');
-  const commitHash = utils
-    .execPipe('git rev-parse --short HEAD')
-    .toString()
-    .replace('\n', '');
-  const canaryVersion = `0.0.0-${commitCount}-${commitHash}`;
+  const canaryVersion = `0.0.0-${commitCount}`;
   utils.exec(`npx lerna version ${canaryVersion} --exact --no-push --yes`);
   utils.exec(
     'npx lerna publish from-package --dist-tag canary --no-verify-access --ignore-scripts --yes'
