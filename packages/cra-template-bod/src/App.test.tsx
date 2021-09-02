d742d287923462d0564fca1984222b68370d4ad1
sabertazimi <sabertazimi@gmail.com>
2021-09-03 05:01:55 +0800

    test(template-bod): rectify app testing with react-router
    issue #19

--- packages/cra-template-bod/src/App.test.tsx
+++ packages/cra-template-bod/src/App.test.tsx
@@ -1,13 +1,16 @@
 import { render } from '@testing-library/react';
 import React from 'react';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import store from './store';
 
 test('renders learn react link', () => {
   const { getByText } = render(
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   );
 
