89d294698285a08871345b2a53f42dfa25c2ca5e
sabertazimi <sabertazimi@gmail.com>
2021-09-05 03:54:49 +0800

    build(Jest): rectify Jest projects configuration

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  projects: ['<rootDir>/packages/bod/jest.config.js'],
+  projects: ['<rootDir>/packages/bod'],
   collectCoverage: true,
   coverageDirectory: '<rootDir>/coverage',
   coverageReporters: ['json-summary', 'lcov', 'text', 'clover'],
