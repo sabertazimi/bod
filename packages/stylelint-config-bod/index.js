47f13276be041d1dbc47cd428d5077e0556433e9
sabertazimi <sabertazimi@gmail.com>
2021-10-23 22:16:13 +0800

    fix(stylelint-bod): disable `scss/no-global-function-names` rule

--- packages/stylelint-config-bod/index.js
+++ packages/stylelint-config-bod/index.js
@@ -41,6 +41,9 @@ module.exports = {
     {
       files: ['*.scss', '**/*.scss'],
       extends: ['stylelint-config-standard-scss'],
+      rules: {
+        'scss/no-global-function-names': false,
+      },
     },
   ],
 };
