d506aac0a8849af85a37896461c3501929840ee5
sabertazimi <sabertazimi@gmail.com>
2021-12-03 21:14:49 +0800

    fix(eslint-vue): disable `vue/multi-word-component-names`

--- packages/eslint-config-vue/index.js
+++ packages/eslint-config-vue/index.js
@@ -18,7 +18,7 @@ module.exports = {
   extends: ['plugin:vue/vue3-recommended', '@dg-scripts/eslint-config-ts'],
   rules: {
     'vue/max-attributes-per-line': ['warn', { singleline: 5 }],
-    'vue/html-self-closing': 'off',
+    'vue/multi-word-component-names': 'off',
     'vue/no-v-html': 'off',
     'vue/require-prop-types': 'off',
     'vue/require-default-prop': 'off',
