bfa4188df636254f9728f0d4ac8f5260f03ee439
sabertazimi <sabertazimi@gmail.com>
2021-12-04 18:18:53 +0800

    fix(CRA-types): update type definition for SVG components

--- packages/react-scripts/lib/react-app.d.ts
+++ packages/react-scripts/lib/react-app.d.ts
@@ -47,12 +47,11 @@ declare module '*.webp' {
 declare module '*.svg' {
   import * as React from 'react';
 
-  export const ReactComponent: React.FunctionComponent<
+  const ReactComponent: React.FunctionComponent<
     React.SVGProps<SVGSVGElement> & { title?: string }
   >;
 
-  const src: string;
-  export default src;
+  export default ReactComponent;
 }
 
 declare module '*.module.css' {
