b926c266e2acc24797b953e99acc93ab8cd0cabd
sabertazimi <sabertazimi@gmail.com>
2022-04-02 18:54:25 +0800

    fix(template-react): switch `render` to `createRoot` function

--- packages/cra-template-bod/src/index.tsx
+++ packages/cra-template-bod/src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
@@ -7,15 +7,14 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import store from './stores';
 
-ReactDOM.render(
+createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/bod/bod/">
         <App />
       </BrowserRouter>
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
