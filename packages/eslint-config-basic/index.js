1827ee5316c963c02c8e8654bb9fdd8c3d29179a
sabertazimi <sabertazimi@gmail.com>
2022-04-03 18:08:53 +0800

    fix(eslint-config): `eslint-plugin-import` support TypeScript

--- packages/eslint-config-basic/index.js
+++ packages/eslint-config-basic/index.js
@@ -33,7 +33,7 @@ module.exports = {
   plugins: ['html', 'unicorn'],
   settings: {
     'import/resolver': {
-      node: { extensions: ['.js', '.mjs', '.ts', '.d.ts'] },
+      node: { extensions: ['.js', '.mjs'] },
     },
   },
   overrides: [
