89e78703518a725b7bb5195abbb3dc6164673064
Sabertaz <sabertazimi@gmail.com>
2022-04-16 06:56:49 +0800

    feat(template-testing): add AXE a11y testing (#698)

--- packages/cra-template-bod/src/components/Counter/__tests__/index.test.tsx
+++ packages/cra-template-bod/src/components/Counter/__tests__/index.test.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import { render } from '@testing-library/react';
+import { axe } from 'jest-axe';
 import { create } from 'react-test-renderer';
 import Counter from '../index';
 
 describe('Counter', () => {
   test('should render correctly (snapshot)', () => {
     const mockClickHandler = jest.fn();
+
     const tree = create(
       <Counter
         count={0}
@@ -17,6 +19,27 @@ describe('Counter', () => {
         onIncrementIfOdd={mockClickHandler}
       />
     ).toJSON();
+
     expect(tree).toMatchSnapshot();
+  });
+
+  test('Should render accessibility guidelines (AXE)', async () => {
+    const mockClickHandler = jest.fn();
+    const { container } = render(
+      <Counter
+        count={0}
+        incrementAmount={'2'}
+        onIncrementAmountChange={mockClickHandler}
+        onDecrement={mockClickHandler}
+        onIncrement={mockClickHandler}
+        onIncrementAsync={mockClickHandler}
+        onIncrementByAmount={mockClickHandler}
+        onIncrementIfOdd={mockClickHandler}
+      />
+    );
+
+    const results = await axe(container);
+
+    expect(results).toHaveNoViolations();
   });
 });
--- packages/cra-template/template/src/App.test.js
+++ packages/cra-template/template/src/App.test.js
@@ -1,8 +1,21 @@
 import { render, screen } from '@testing-library/react';
+import { axe } from 'jest-axe';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  test('renders learn react link', () => {
+    render(<App />);
+
+    const linkElement = screen.getByText(/learn react/i);
+
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test('Should render accessibility guidelines (AXE)', async () => {
+    const { container } = render(<App />);
+
+    const results = await axe(container);
+
+    expect(results).toHaveNoViolations();
+  });
 });
