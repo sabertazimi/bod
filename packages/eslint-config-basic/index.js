1b2a1b76efef42a7752fb655f1076f607121b1b3
sabertazimi <sabertazimi@gmail.com>
2022-05-12 22:20:47 +0800

    fix(eslint-config): add missing JSON extension rules

--- packages/eslint-config-basic/index.js
+++ packages/eslint-config-basic/index.js
@@ -41,9 +41,26 @@ module.exports = {
       files: ['*.json', '*.json5'],
       parser: 'jsonc-eslint-parser',
       rules: {
+        'jsonc/array-bracket-spacing': ['error', 'never'],
+        'jsonc/comma-dangle': ['error', 'never'],
+        'jsonc/comma-style': ['error', 'last'],
+        'jsonc/indent': ['error', 2],
+        'jsonc/key-spacing': [
+          'error',
+          { beforeColon: false, afterColon: true },
+        ],
+        'jsonc/no-octal-escape': 'error',
+        'jsonc/object-curly-newline': [
+          'error',
+          { multiline: true, consistent: true },
+        ],
+        'jsonc/object-curly-spacing': ['error', 'always'],
+        'jsonc/object-property-newline': [
+          'error',
+          { allowMultiplePropertiesPerLine: true },
+        ],
         'jsonc/quotes': ['error', 'double'],
         'jsonc/quote-props': ['error', 'always'],
-        'jsonc/comma-dangle': ['error', 'never'],
       },
     },
     {
