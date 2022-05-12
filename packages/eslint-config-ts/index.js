e5ad1e6f5dd63d2b898673e968db168916573dd0
Sabertaz <sabertazimi@gmail.com>
2022-05-12 22:48:02 +0800

    fix(eslint-config): complete typescript overrides (#724)

--- packages/eslint-config-ts/index.js
+++ packages/eslint-config-ts/index.js
@@ -16,7 +16,6 @@ module.exports = {
     'import/named': 'off',
 
     // TS
-    '@typescript-eslint/semi': ['error', 'always'],
     '@typescript-eslint/ban-ts-comment': [
       'error',
       { 'ts-ignore': 'allow-with-description' },
@@ -57,13 +56,52 @@ module.exports = {
     '@typescript-eslint/comma-dangle': ['error', 'always-multiline'],
     'object-curly-spacing': 'off',
     '@typescript-eslint/object-curly-spacing': ['error', 'always'],
+    semi: 'off',
+    '@typescript-eslint/semi': ['error', 'always'],
+    quotes: 'off',
+    '@typescript-eslint/quotes': ['error', 'single'],
+    'space-before-blocks': 'off',
+    '@typescript-eslint/space-before-blocks': ['error', 'always'],
+    'space-before-function-paren': 'off',
+    '@typescript-eslint/space-before-function-paren': [
+      'error',
+      {
+        anonymous: 'always',
+        named: 'never',
+        asyncArrow: 'always',
+      },
+    ],
+    'space-infix-ops': 'off',
+    '@typescript-eslint/space-infix-ops': 'error',
+    'keyword-spacing': 'off',
+    '@typescript-eslint/keyword-spacing': [
+      'error',
+      { before: true, after: true },
+    ],
+    'comma-spacing': 'off',
+    '@typescript-eslint/comma-spacing': [
+      'error',
+      { before: false, after: true },
+    ],
+    'no-extra-parens': 'off',
+    '@typescript-eslint/no-extra-parens': ['error', 'functions'],
+    'no-dupe-class-members': 'off',
+    '@typescript-eslint/no-dupe-class-members': 'error',
+    'no-loss-of-precision': 'off',
+    '@typescript-eslint/no-loss-of-precision': 'error',
+    'lines-between-class-members': 'off',
+    '@typescript-eslint/lines-between-class-members': [
+      'error',
+      'always',
+      { exceptAfterSingleLine: true },
+    ],
 
     // off
-    '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/naming-convention': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-member-accessibility': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-parameter-properties': 'off',
+    '@typescript-eslint/parameter-properties': 'off',
     '@typescript-eslint/no-empty-interface': 'off',
     '@typescript-eslint/ban-ts-ignore': 'off',
     '@typescript-eslint/no-empty-function': 'off',
