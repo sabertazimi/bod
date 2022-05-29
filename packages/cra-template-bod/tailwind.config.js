027df00c9b3e7e9a9b1ac66ce663bcf251c62de0
Sabertaz <sabertazimi@gmail.com>
2022-05-29 17:26:13 +0800

    fix(tailwind-config): remove legacy tailwind configuration (#748)

--- packages/cra-template-bod/tailwind.config.js
+++ packages/cra-template-bod/tailwind.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  mode: 'jit',
   content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
