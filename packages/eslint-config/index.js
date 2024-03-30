b29187caf8a13736d40f94893376fbf24b80565c
sabertazimi <sabertazimi@gmail.com>
2024-03-30 14:06:22 +0800

    feat(eslint-config): add optional Next.js support
    Next.js eslint configuration auto-detection.

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,7 +1,21 @@
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { isPackageExists } from 'local-pkg'
 import antfu from '@antfu/eslint-config'
+import { FlatCompat } from '@eslint/eslintrc'
+
+const baseDirectory = path.dirname(fileURLToPath(import.meta.url))
+
+const compat = new FlatCompat({
+  baseDirectory,
+  resolvePluginsRelativeTo: baseDirectory,
+})
 
 export default antfu(
   {
+    typescript: {
+      tsconfigPath: 'tsconfig.json',
+    },
     react: true,
     formatters: {
       css: true,
@@ -9,6 +23,16 @@ export default antfu(
       markdown: 'prettier',
     },
   },
+  isPackageExists('next') && isPackageExists('eslint-config-next')
+    ? compat.config({
+      overrides: [
+        {
+          files: '**/*.{ts,tsx}',
+          extends: 'next/core-web-vitals',
+        },
+      ],
+    })
+    : {},
   {
     rules: {
       'ts/prefer-literal-enum-member': [
