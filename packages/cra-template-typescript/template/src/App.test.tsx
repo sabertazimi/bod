09710b602da3525fe7c75b66240f0d269ca85987
sabertazimi <sabertazimi@gmail.com>
2024-03-30 10:58:52 +0800

    fix(templates): rectify linters error

--- packages/cra-template-typescript/template/src/App.test.tsx
+++ packages/cra-template-typescript/template/src/App.test.tsx
@@ -7,7 +7,7 @@ describe('App', () => {
   test('renders learn react link', () => {
     render(<App />)
 
-    const linkElement = screen.getByText(/learn react/i)
+    const linkElement = screen.getByText(/learn react/iu)
 
     expect(linkElement).toBeInTheDocument()
   })
