4bb90c01b63bbd4ee6177cf40a54603748d155e7
sabertazimi <sabertazimi@gmail.com>
2024-03-30 10:23:49 +0800

    fix(react-scripts): rectify ReferenceError

--- packages/react-scripts/scripts/build.js
+++ packages/react-scripts/scripts/build.js
@@ -14,7 +14,7 @@ process.env.NODE_ENV = 'production'
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
 // terminate the Node.js process with a non-zero exit code.
-process.on('unhandledRejection', err => {
+process.on('unhandledRejection', (err) => {
   throw err
 })
 
@@ -32,6 +32,12 @@ const printHostingInstructions = require('react-dev-utils/printHostingInstructio
 const FileSizeReporter = require('react-dev-utils/FileSizeReporter')
 const printBuildError = require('react-dev-utils/printBuildError')
 
+// We require that you explicitly set browsers and do not fall back to
+// browserslist defaults.
+const { checkBrowsers } = require('react-dev-utils/browsersHelper')
+const paths = require('../config/paths')
+const configFactory = require('../config/webpack.config')
+
 const measureFileSizesBeforeBuild = FileSizeReporter.measureFileSizesBeforeBuild
 const printFileSizesAfterBuild = FileSizeReporter.printFileSizesAfterBuild
 const useYarn = fs.existsSync(paths.yarnLockFile)
@@ -43,9 +49,8 @@ const WARN_AFTER_CHUNK_GZIP_SIZE = 1024 * 1024
 const isInteractive = process.stdout.isTTY
 
 // Warn and crash if required files are missing
-if (!checkRequiredFiles([paths.appHtml, paths.appIndexJs])) {
+if (!checkRequiredFiles([paths.appHtml, paths.appIndexJs]))
   process.exit(1)
-}
 
 const argv = process.argv.slice(2)
 const writeStatsJson = argv.includes('--stats')
@@ -53,18 +58,13 @@ const writeStatsJson = argv.includes('--stats')
 // Generate configuration
 const config = configFactory('production')
 
-// We require that you explicitly set browsers and do not fall back to
-// browserslist defaults.
-const { checkBrowsers } = require('react-dev-utils/browsersHelper')
-const paths = require('../config/paths')
-const configFactory = require('../config/webpack.config')
 checkBrowsers(paths.appPath, isInteractive)
   .then(() => {
     // First, read the current file sizes in build directory.
     // This lets us display how much they changed later.
     return measureFileSizesBeforeBuild(paths.appBuild)
   })
-  .then(previousFileSizes => {
+  .then((previousFileSizes) => {
     // Remove all content but keep the directory so that
     // if you're in it, you don't end up in Trash
     fs.emptyDirSync(paths.appBuild)
@@ -80,15 +80,16 @@ checkBrowsers(paths.appPath, isInteractive)
         console.log(warnings.join('\n\n'))
         console.log(
           `\nSearch for the ${chalk.underline(
-            chalk.yellow('keywords')
-          )} to learn more about each warning.`
+            chalk.yellow('keywords'),
+          )} to learn more about each warning.`,
         )
         console.log(
           `To ignore, add ${chalk.cyan(
-            '// eslint-disable-next-line'
-          )} to the line before.\n`
+            '// eslint-disable-next-line',
+          )} to the line before.\n`,
         )
-      } else {
+      }
+      else {
         console.log(chalk.green('Compiled successfully.\n'))
       }
 
@@ -98,7 +99,7 @@ checkBrowsers(paths.appPath, isInteractive)
         previousFileSizes,
         paths.appBuild,
         WARN_AFTER_BUNDLE_GZIP_SIZE,
-        WARN_AFTER_CHUNK_GZIP_SIZE
+        WARN_AFTER_CHUNK_GZIP_SIZE,
       )
       console.log()
 
@@ -111,29 +112,30 @@ checkBrowsers(paths.appPath, isInteractive)
         publicUrl,
         publicPath,
         buildFolder,
-        useYarn
+        useYarn,
       )
     },
-    err => {
+    (err) => {
       const tscCompileOnError = process.env.TSC_COMPILE_ON_ERROR === 'true'
       if (tscCompileOnError) {
         console.log(
           chalk.yellow(
-            'Compiled with the following type errors (you may want to check these before deploying your app):\n'
-          )
+            'Compiled with the following type errors (you may want to check these before deploying your app):\n',
+          ),
         )
         printBuildError(err)
-      } else {
+      }
+      else {
         console.log(chalk.red('Failed to compile.\n'))
         printBuildError(err)
         process.exit(1)
       }
-    }
+    },
   )
-  .catch(err => {
-    if (err && err.message) {
+  .catch((err) => {
+    if (err && err.message)
       console.log(err.message)
-    }
+
     process.exit(1)
   })
 
@@ -146,50 +148,49 @@ function build(previousFileSizes) {
     compiler.run((err, stats) => {
       let messages
       if (err) {
-        if (!err.message) {
+        if (!err.message)
           return reject(err)
-        }
 
         let errMessage = err.message
 
         // Add additional information for postcss errors
-        if (Object.prototype.hasOwnProperty.call(err, 'postcssNode')) {
+        if (Object.prototype.hasOwnProperty.call(err, 'postcssNode'))
           errMessage += `\nCompileError: Begins at CSS selector ${err.postcssNode.selector}`
-        }
 
         messages = formatWebpackMessages({
           errors: [errMessage],
           warnings: [],
         })
-      } else {
+      }
+      else {
         messages = formatWebpackMessages(
-          stats.toJson({ all: false, warnings: true, errors: true })
+          stats.toJson({ all: false, warnings: true, errors: true }),
         )
       }
       if (messages.errors.length) {
         // Only keep the first error. Others are often indicative
         // of the same problem, but confuse the reader with noise.
-        if (messages.errors.length > 1) {
+        if (messages.errors.length > 1)
           messages.errors.length = 1
-        }
+
         return reject(new Error(messages.errors.join('\n\n')))
       }
       if (
-        process.env.CI &&
-        (typeof process.env.CI !== 'string' ||
-          process.env.CI.toLowerCase() !== 'false') &&
-        messages.warnings.length
+        process.env.CI
+        && (typeof process.env.CI !== 'string'
+        || process.env.CI.toLowerCase() !== 'false')
+        && messages.warnings.length
       ) {
         // Ignore sourcemap warnings in CI builds. See #8227 for more info.
         const filteredWarnings = messages.warnings.filter(
-          w => !/Failed to parse source map/.test(w)
+          w => !/Failed to parse source map/.test(w),
         )
         if (filteredWarnings.length) {
           console.log(
             chalk.yellow(
-              '\nTreating warnings as errors because process.env.CI = true.\n' +
-                'Most CI servers set it automatically.\n'
-            )
+              '\nTreating warnings as errors because process.env.CI = true.\n'
+              + 'Most CI servers set it automatically.\n',
+            ),
           )
           return reject(new Error(filteredWarnings.join('\n\n')))
         }
