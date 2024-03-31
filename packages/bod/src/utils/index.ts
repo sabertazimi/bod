7bbb119ee02397b9070b86e39d3299f723c0d0f7
sabertazimi <sabertazimi@gmail.com>
2024-03-31 21:45:49 +0800

    feat(bod): add vue and vite template generation

--- packages/bod/src/utils/index.ts
+++ packages/bod/src/utils/index.ts
@@ -1,5 +1,18 @@
+import process from 'node:process'
 import { color, printer } from './console'
 import { inquirer, program } from './core'
 import { envinfo, spawn } from './os'
 
-export { color, printer, inquirer, program, envinfo, spawn }
+function findPackageManager(): string {
+  const userAgent = process.env.npm_config_user_agent ?? ''
+  const packageManager = /pnpm/.test(userAgent)
+    ? 'pnpm'
+    : /yarn/.test(userAgent)
+      ? 'yarn'
+      : /bun/.test(userAgent)
+        ? 'bun'
+        : 'npm'
+  return packageManager
+}
+
+export { color, envinfo, findPackageManager, inquirer, printer, program, spawn }
