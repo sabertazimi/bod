c98993b36b3414b02670910e2309b70de240e95e
Sabertaz <sabertazimi@gmail.com>
2024-04-01 04:31:54 +0800

    test(bod-cli): add tests for utils (#1155)

--- packages/bod/src/utils/__tests__/index.test.ts
+++ packages/bod/src/utils/__tests__/index.test.ts
@@ -1,4 +1,5 @@
-import { color, program } from '../index'
+import process from 'node:process'
+import { color, findPackageManager, program } from '../index'
 
 describe('utils', () => {
   it('should execute chalk correctly', () => {
@@ -9,5 +10,22 @@ describe('utils', () => {
     const mockParse = jest.spyOn(program, 'parse').mockImplementation(jest.fn())
     expect(process.env.__BOD__).toStrictEqual('__BOD__')
     mockParse.mockRestore()
+  })
+
+  it('should return correct package manager', () => {
+    process.env.npm_config_user_agent = 'pnpm/8.15.0'
+    expect(findPackageManager()).toStrictEqual('pnpm')
+
+    process.env.npm_config_user_agent = 'yarn/1.22.22'
+    expect(findPackageManager()).toStrictEqual('yarn')
+
+    process.env.npm_config_user_agent = 'yarn/4.2.0'
+    expect(findPackageManager()).toStrictEqual('yarn')
+
+    process.env.npm_config_user_agent = 'bun/0.1.0'
+    expect(findPackageManager()).toStrictEqual('bun')
+
+    process.env.npm_config_user_agent = 'npm/10.0.0'
+    expect(findPackageManager()).toStrictEqual('npm')
   })
 })
