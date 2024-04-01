6a640cccaf6e63746f105199161849f556b4d9b1
sabertazimi <sabertazimi@gmail.com>
2024-04-02 07:27:42 +0800

    feat(eslint-config): add testing-library support

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,8 +1,9 @@
 // @ts-check
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
-import antfu, { GLOB_MARKDOWN, GLOB_MARKDOWN_CODE, GLOB_TS, GLOB_TSX } from '@antfu/eslint-config'
+import antfu, { GLOB_MARKDOWN, GLOB_MARKDOWN_CODE, GLOB_TESTS, GLOB_TS, GLOB_TSX } from '@antfu/eslint-config'
 import { FlatCompat } from '@eslint/eslintrc'
+import eslintPluginTestingLibrary from 'eslint-plugin-testing-library'
 import { isPackageExists } from 'local-pkg'
 
 const baseDirectory = path.dirname(fileURLToPath(import.meta.url))
@@ -31,15 +32,57 @@ const eslintConfigMarkdown = {
 }
 
 /** @type {import('@antfu/eslint-config').TypedFlatConfigItem} */
-const eslintConfigRules = {
+const eslintConfigTestingLibrary = {
+  files: GLOB_TESTS,
+  plugins: {
+    'testing-library': eslintPluginTestingLibrary,
+  },
   rules: {
-    'style/brace-style': ['error', '1tbs'],
+    'test/no-interpolation-in-snapshots': 'error',
+    'test/no-mocks-import': 'error',
     'test/prefer-lowercase-title': [
       'error',
       {
         ignoreTopLevelDescribe: true,
       },
     ],
+    'test/valid-describe-callback': 'error',
+    'test/valid-expect': 'error',
+    'test/valid-title': 'warn',
+    'testing-library/await-async-events': [
+      'error',
+      { eventModule: 'userEvent' },
+    ],
+    'testing-library/await-async-queries': 'error',
+    'testing-library/await-async-utils': 'error',
+    'testing-library/no-await-sync-events': [
+      'error',
+      { eventModules: ['fire-event'] },
+    ],
+    'testing-library/no-await-sync-queries': 'error',
+    'testing-library/no-container': 'error',
+    'testing-library/no-debugging-utils': 'warn',
+    'testing-library/no-dom-import': 'error',
+    'testing-library/no-global-regexp-flag-in-query': 'error',
+    'testing-library/no-node-access': 'error',
+    'testing-library/no-promise-in-fire-event': 'error',
+    'testing-library/no-render-in-lifecycle': 'error',
+    'testing-library/no-unnecessary-act': 'error',
+    'testing-library/no-wait-for-multiple-assertions': 'error',
+    'testing-library/no-wait-for-side-effects': 'error',
+    'testing-library/no-wait-for-snapshot': 'error',
+    'testing-library/prefer-find-by': 'error',
+    'testing-library/prefer-presence-queries': 'error',
+    'testing-library/prefer-query-by-disappearance': 'error',
+    'testing-library/prefer-screen-queries': 'error',
+    'testing-library/render-result-naming-convention': 'error',
+  },
+}
+
+/** @type {import('@antfu/eslint-config').TypedFlatConfigItem} */
+const eslintConfigRules = {
+  rules: {
+    'style/brace-style': ['error', '1tbs'],
     'ts/prefer-literal-enum-member': [
       'error',
       {
@@ -50,7 +93,12 @@ const eslintConfigRules = {
 }
 
 /** @type {import('@antfu/eslint-config').TypedFlatConfigItem[]} */
-const eslintConfig = [eslintConfigNext, eslintConfigMarkdown, eslintConfigRules]
+const eslintConfig = [
+  eslintConfigNext,
+  eslintConfigMarkdown,
+  eslintConfigTestingLibrary,
+  eslintConfigRules,
+]
 
 /** @type {import('@antfu/eslint-config').OptionsConfig} */
 const eslintConfigAntfu = {
